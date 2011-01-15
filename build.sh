b20610c0f6938ca0ba95acb6b01d81eadf240b20
Patrick Rapin <toupie300@gmail.com>
2011-01-15 16:11:34 +0100

    Built luasocket

--- build.sh
+++ build.sh
@@ -13,3 +13,13 @@ cd ../luaposix-5.1.7
 export C_INCLUDE_PATH=$DISTRIB_ROOT/include:$C_INCLUDE_PATH
 make
 cp posix.so $LUA_LIB
+cd ../luafilesystem-1.5.0
+make
+cp src/lfs.so $LUA_LIB
+cd ../luasocket-2.0.2
+make
+mkdir -p $LUA_LIB/socket
+mkdir -p $LUA_LIB/mime
+cp src/http.lua src/url.lua src/tp.lua src/ftp.lua src/smtp.lua src/socket.so $LUA_LIB/socket
+cp src/ltn12.lua src/socket.lua src/mime.lua   $LUA_LIB
+cp src/mime.so  $LUA_LIB/mime
