ceab2f3d1b3385e2c2256a4f26a3249558b6b40c
Patrick Rapin <toupie300@gmail.com>
2011-01-15 14:21:46 +0100

    Build luaposix and luafilesystem

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
-#!/bin/sh
-DEST_DIR=../distrib/root/usr
+#!/bin/bash
+DISTRIB_ROOT=$PWD/distrib/root/usr
+LUA_LIB=$DISTRIB_ROOT/share/lua/5.1
 cd lua-5.1.4/
 make
-mkdir -p $DEST_DIR/bin
-mkdir -p $DEST_DIR/include
+mkdir -p $DISTRIB_ROOT/bin
+mkdir -p $DISTRIB_ROOT/include
+mkdir -p $LUA_LIB
 strip src/lua src/luac
-cp src/lua src/luac $DEST_DIR/bin
-cp src/lua.h src/luaconf.h src/lualib.h src/lauxlib.h etc/lua.hpp $DEST_DIR/include
+cp src/lua src/luac $DISTRIB_ROOT/bin
+cp src/lua.h src/luaconf.h src/lualib.h src/lauxlib.h etc/lua.hpp $DISTRIB_ROOT/include
+cd ../luaposix-5.1.7
+export C_INCLUDE_PATH=$DISTRIB_ROOT/include:$C_INCLUDE_PATH
+make
+cp posix.so $LUA_LIB
